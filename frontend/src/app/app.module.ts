964cfd7d31538d36d5f006f0a3d3a9f00d47a33f
Bob Fornal <bob.fornal@leadingedje.com>
2023-03-05 11:36:56 -0500

    Working Session #4 Code

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,12 +10,14 @@ import { HeaderComponent } from './features/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
+
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiHandlerService } from '../../core/services/api-handler.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private api: ApiHandlerService) {
+    api.test.subscribe(this.handler.bind(this));
+  }
+
+  ngOnInit(): void {
+    this.api.getTest();
+  }
+
+  handler = (data: any) => {
+    console.log(data);
+  }
 }
