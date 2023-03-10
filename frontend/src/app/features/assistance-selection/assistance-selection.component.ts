a03fbef5bd96e61fa44df83051a75d63b9ff2c94
Dillon Hess <dhess268@gmail.com>
2023-03-10 00:48:05 -0500

    add interface for assistance level

--- frontend/src/app/features/assistance-selection/assistance-selection.component.ts
+++ frontend/src/app/features/assistance-selection/assistance-selection.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import config from "../../core/constants/config.json"
+import { AssistanceLevel } from '../../core/interfaces/assistance-level';
+
 @Component({
   selector: 'assistance-selection',
   templateUrl: './assistance-selection.component.html',
   styleUrls: ['./assistance-selection.component.scss']
 })
 export class AssistanceSelectionComponent {
-  data: any = config['assistance-levels']
+  data: AssistanceLevel[] = config['assistance-levels']
+  selectedLevel: string = ''
+
+  handleSelectLevel= (level: any) : void => {
+    this.selectedLevel = level.key
+    console.log(this.selectedLevel)
+  }
 }
