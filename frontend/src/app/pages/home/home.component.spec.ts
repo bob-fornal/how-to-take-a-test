278c7a37833b52f692f813fe19f146d43960b888
Catalin <csiegling@gmail.com>
2023-03-10 19:03:11 -0600

    Update home.component.spec.ts

--- frontend/src/app/pages/home/home.component.spec.ts
+++ frontend/src/app/pages/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { GradeLevelSelectionComponent } from 'src/app/features/grade-level-selection/grade-level-selection.component';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent, GradeLevelSelectionComponent ]
     })
     .compileComponents();
 
