08cf1dfe2eee141dd0534bc2dbc65c0d8bbcdeee
Dillon Hess <dhess268@gmail.com>
2023-03-11 11:59:41 -0500

    Change import order for readability

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,14 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
+
+import { AssistanceSelectionComponent } from './features/assistance-selection/assistance-selection.component';
+import { GradeLevelSelectionComponent } from './features/grade-level-selection/grade-level-selection.component';
 import { HeaderComponent } from './features/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 import { MatButtonModule } from '@angular/material/button'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
-import { AssistanceSelectionComponent } from './features/assistance-selection/assistance-selection.component';
-import { GradeLevelSelectionComponent } from './features/grade-level-selection/grade-level-selection.component';
 
 @NgModule({
   declarations: [
