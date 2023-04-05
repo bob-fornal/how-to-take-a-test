9dd79430b1ddcc48ea7e2791932bd25967ab74ea
Catalin <csiegling@gmail.com>
2023-04-05 18:34:05 -0500

    initialization of switch
    gross implementation. working.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'HTTAT';
+  isDarkMode = false;
+
+  constructor(private overlayContainer: OverlayContainer) {}
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      this.overlayContainer.getContainerElement().classList.add('test');
+      document.documentElement.classList.add('test');
+    } else {
+      this.overlayContainer.getContainerElement().classList.remove('test');
+      document.documentElement.classList.remove('test');
+    }
+  }
 }
