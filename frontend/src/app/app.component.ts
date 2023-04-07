5a15a9f3182f847110bacb6cc2c2b40141199d7e
Catalin <csiegling@gmail.com>
2023-04-07 18:30:48 -0500

    darkmode uses LH. switch is material and moved.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,7 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  isDarkMode = false;
-
-  constructor(@Inject(DOCUMENT) private document: Document) {}
-
-  toggleDarkMode() {
-    this.isDarkMode = !this.isDarkMode;
-    if (this.isDarkMode) {
-      this.document.documentElement.classList.add('dark-mode');
-    } else {
-      this.document.documentElement.classList.remove('dark-mode');
-    }
-  }
-}
+export class AppComponent {}
