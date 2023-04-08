275465f3c56ab358b65301aa71ee721603759127
Dillon Hess <dhess268@gmail.com>
2023-04-08 17:17:03 -0400

    Add semicolon

--- backend/routes/index.routes.js
+++ backend/routes/index.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 
-const authRouter = require('./auth.routes')
+const authRouter = require('./auth.routes');
 
 const router = express.Router();
 
